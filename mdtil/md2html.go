de59e5e63382da07441a537ec8e615551a2b65f4
TruthHun <TruthHun@QQ.COM>
2017-12-13 19:48:32 +0800

    markdown2html

---
+++ mdtil/md2html.go
@@ -0,0 +1,11 @@
+package mdtil
+
+import "github.com/russross/blackfriday"
+
+//将markdown内容转成html内容
+//@param            MarkdownContent     markdown文本内容
+//@return           html                转化后的html
+func Md2html(MarkdownContent string) (html string) {
+	out := blackfriday.MarkdownCommon([]byte(MarkdownContent))
+	return string(out)
+}
