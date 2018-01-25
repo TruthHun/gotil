ec6492c678b3969cee28c926893074d6282f43ed
truthhun <truthhun@qq.com>
2018-01-25 17:27:09 +0800

    测测试

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/TruthHun/gotil/util"
+)
+
+func main() {
+	//f := "https://cover.kancloud.cn/johng/gf!middle"
+	f := "https://pic2.zhimg.com/v2-d6010fa144e5647c38ebfe6a93fd619f_b.jpg"
+	fmt.Println(util.CrawlFile(f, "./", 10))
 }
